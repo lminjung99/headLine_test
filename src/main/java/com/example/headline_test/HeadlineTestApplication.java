f64c4f38b160975adb94f83b0bf1672ab8d79b8c
lminjung99 <lminjung99@konkuk.ac.kr>
2023-10-15 09:31:32 +0900

    print root dir

--- src/main/java/com/example/headline_test/HeadlineTestApplication.java
+++ src/main/java/com/example/headline_test/HeadlineTestApplication.java
@@ -1,5 +1,8 @@
 package com.example.headline_test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HeadlineTestApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(HeadlineTestApplication.class, args);
+		Path currentWorkingDir = Paths.get("").toAbsolutePath();
+		
+		System.out.println("Root dir : "+currentWorkingDir);
+
 	}
 
 }
